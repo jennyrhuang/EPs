ae9a15963406402f8eaf08c5d9976572f0c27314
jenny <91958563+jennyrhuang@users.noreply.github.com>
2022-02-15 23:41:55 -0300

    Add files via upload

---
+++ back-end.js
@@ -0,0 +1,33 @@
+
+
+function alteraCumprimento(elemento) {
+    areaTexto = document.getElementById("textop")
+    areaTexto.innerHTML = "Oi" + elemento.value +", tudo bem?"
+    ele = document.getElementById("entradaNome")
+    ele.hidden = true
+    
+    bot = document.getEelementById("mostraNome")
+    
+    bot.className = "w3-container w3-button w3-blue w3-border w3-block"
+}
+
+
+function mostraInputNome(e) {
+    e.hidden = true
+    
+    ele = document.getElementById("entradaNome")
+    ele.hidden = false
+}
+
+function verifica(ev) {
+    ev.value
+}
+
+function pedeNome() {
+    nome = prompt("Insira seu nome: ")
+    elementoP = document.getElementById("textop")
+    elementoP.innerHTML = "Oi "+nome+", aponte aqui"
+    elementoP.style = "color:blue"
+}
+
+cont express = require(
