54abd88d8895437f2bfe0ed4969e7b7b25d7f81f
jenny <91958563+jennyrhuang@users.noreply.github.com>
2022-02-24 16:30:19 -0300

    Update back-end.js

--- POO/back-end.js
+++ POO/back-end.js
@@ -1,33 +1,87 @@
+function checasenha(evento) {
+    inpsenhaconf = evento.currentTarget
+    inpsenha = document.getElementById("isenha")
+  
+    ls = document.getElementById("lsenha")
+    lc = document.getElementById("lconfirma")
+  
+    if(inpsenha.value != inpsenhaconf.value) {
+      ls.className = "w3-text-red"
+      lc.className = "w3-text-red"
+  
+      inpsenhaconf.value =""
+      inpsenha.value =""
+  
+      document.getElementById("caixaerro").style.display='block'
+      document.getElementById("mensagemerro").innerHTML = "Verifique sua senha"
+      inpsenha.focus()
+      }
+
+    else {
+      ls.className = "w3-text-black"
+      lc.className = "w3-text-black"
+      ls.innerHTML = "Senha OK"
+      inpsenha.className = "w3-hide"
+      inpsenhaconf.className = "w3-hide"
+      }
+    }
+  
+
+    function testaCPF(evento) {
+        var Soma;
+        var Resto;
+        Soma = 0;
+  
+      strCPF = evento.currentTarget.value
+      strCPF = strCPF.replace('.','')
+      strCPF = strCPF.replace('.','')
+      strCPF = strCPF.replace('-','')
+  
+      if (strCPF == "00000000000") corrigeCPF(strCPF);
+  
+      for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+      Resto = (Soma * 10) % 11;
+  
+        if ((Resto == 10) || (Resto == 11))  Resto = 0;
+        if (Resto != parseInt(strCPF.substring(9, 10)) ) corrigeCPF(strCPF);
+  
+      Soma = 0;
+        for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+        Resto = (Soma * 10) % 11;
+  
+        if ((Resto == 10) || (Resto == 11))  Resto = 0;
+        if (Resto != parseInt(strCPF.substring(10, 11) ) ) corrigeCPF(strCPF);
+        return true;
+    }
 
 
-function alteraCumprimento(elemento) {
-    areaTexto = document.getElementById("textop")
-    areaTexto.innerHTML = "Oi" + elemento.value +", tudo bem?"
-    ele = document.getElementById("entradaNome")
-    ele.hidden = true
-    
-    bot = document.getEelementById("mostraNome")
-    
-    bot.className = "w3-container w3-button w3-blue w3-border w3-block"
-}
+  function corrigeCPF(cpf){
+    document.getElementById("caixaerro").style.display='block'
+    document.getElementById("mensagemerro").innerHTML = "CPF inválido: "+cpf
+  
+      inputCPF = document.getElementById("icpf")
+      inputCPF.value=""
+      inputCPF.className = "w3-text-red"
+  }
+  
+  
+    function pegaDadosCep(evento) {
+      const xhttp = new XMLHttpRequest()
+  
+      cpoCep = evento.currentTarget
+  
+      url = "https://viacep.com.br/ws/" + cpoCep.value + "/json"
+      xhttp.onload = dadosCepCarregados
+  
+      xhttp.open("GET", url);
+      xhttp.send();
+    }
+  
 
-
-function mostraInputNome(e) {
-    e.hidden = true
-    
-    ele = document.getElementById("entradaNome")
-    ele.hidden = false
-}
-
-function verifica(ev) {
-    ev.value
-}
-
-function pedeNome() {
-    nome = prompt("Insira seu nome: ")
-    elementoP = document.getElementById("textop")
-    elementoP.innerHTML = "Oi "+nome+", aponte aqui"
-    elementoP.style = "color:blue"
-}
-
-cont express = require(
+    function dadosCepCarregados() {
+        resp = JSON.parse(this.responseText)
+        cpoLogr = document.getElementById("ilogradouro")
+        cpoLogr.value = resp.logradouro
+        cpoLogr.disabled = true
+        document.getElementById("inum").focus()
+    }
